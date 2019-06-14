feca5fb368beb58ec64a11b816feda745db24c64
Lee Gainer <piealacode@gmail.com>
2019-06-14 18:31:14 -0400

    Added content to data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      products: [
+        { name: 'Banana', price: 20},
+        { name: 'Tomatoes', price: 30},
+        { name: 'Cherries', price: 15},
+        { name: 'Corn', price: 25},
+      ]
     }
   }
 }
-</style>
+</script>
+
+
+<style lang="scss"></style>
