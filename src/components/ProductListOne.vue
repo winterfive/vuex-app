0f52d76e4d18379ca583e3dd43a25626d782a0a2
Lee Gainer <piealacode@gmail.com>
2019-06-17 17:38:35 -0400

    Set sale prices for list one

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,7 +2,7 @@
   <div class="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li v-for="product in products" v-bind:key="product.name">
+      <li v-for="product in saleProducts" v-bind:key="product.name">
         <span class="name">{{ product.name }}</span>
         <span class="price"> ${{ product.price }}</span>
       </li>
@@ -15,6 +15,15 @@ export default {
   computed: {
     products() {
       return this.$store.state.products;
+    },
+    saleProducts() {
+      let saleProducts = this.$store.state.products.map(product => {
+        return {
+          name: "**" + product.name + "**",
+          price: Math.round(product.price / 2)
+        };
+      });
+      return saleProducts;
     }
   }
 };
