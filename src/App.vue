a77cbc1447a50ab56593496a4b6a8073520238bf
Lee Gainer <piealacode@gmail.com>
2019-06-17 13:52:53 -0400

    Created more content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <product-list-one></product-list-one>
-    <product-list-two></product-list-two>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+
 export default {
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
   name: "app",
   data() {
     return {
@@ -20,6 +27,5 @@ export default {
   }
 };
 </script>
-
 
 <style lang="scss"></style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="product-list-two">
+    <h2>Product List Two</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+#product-list-two {
+  background: red;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+
+.product-list-two ul {
+  padding: 0;
+}
+
+.product-list-two li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+
+.price {
+  font-weight: bold;
+  color: black;
+}
+</style>
